134e04bd9227d62de236268d246bbf7774fa33ec
Andrey Yantsen <andrey@janzen.su>
2020-01-06 20:33:37 +0000

    convert enumValues in settings to vectors

--- plex-api/src/media_container/settings.rs
+++ plex-api/src/media_container/settings.rs
@@ -1,4 +1,6 @@
+use crate::serde_helpers::option_pipe_separated_to_vec;
 use crate::MediaContainer;
+use thiserror::Error;
 
 #[derive(Debug, Deserialize, Clone)]
 #[cfg_attr(all(test, feature = "test_new_attributes"), serde(deny_unknown_fields))]
@@ -29,6 +31,65 @@ pub struct Setting {
     payload: Payload,
 }
 
+#[derive(Error, Debug)]
+pub enum SettingParsingError {
+    #[error("Provided string incorrectly formatted")]
+    IncorrectFormat,
+    #[error("Unable to convert provided key to integer")]
+    UnableToParseInt,
+}
+
+#[derive(Debug, Clone)]
+pub struct SettingEnumValueString(String, String);
+
+impl std::str::FromStr for SettingEnumValueString {
+    type Err = SettingParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value: Vec<_> = s.split(':').collect();
+
+        match value.len() {
+            1 => Ok(SettingEnumValueString(String::from(s), String::from(s))),
+            2 => Ok(SettingEnumValueString(
+                String::from(value[0]),
+                String::from(value[1]),
+            )),
+            _ => Err(SettingParsingError::IncorrectFormat),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SettingEnumValueInt(i32, String);
+
+// TODO: Better error conversion
+impl From<std::num::ParseIntError> for SettingParsingError {
+    fn from(e: std::num::ParseIntError) -> Self {
+        println!("{:#?}", e);
+        Self::UnableToParseInt
+    }
+}
+
+impl std::str::FromStr for SettingEnumValueInt {
+    type Err = SettingParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value: Vec<_> = s.split(':').collect();
+
+        match value.len() {
+            1 => Ok(SettingEnumValueInt(
+                String::from(s).parse()?,
+                String::from(s),
+            )),
+            2 => Ok(SettingEnumValueInt(
+                value[0].parse()?,
+                String::from(value[1]),
+            )),
+            _ => Err(SettingParsingError::IncorrectFormat),
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Clone)]
 #[serde(tag = "type", rename_all = "lowercase")]
 pub enum Payload {
@@ -39,14 +100,22 @@ pub enum Payload {
     Int {
         default: i32,
         value: i32,
-        #[serde(rename = "enumValues")]
-        enum_values: Option<String>, // TODO: decode values to hash/vec
+        #[serde(
+            rename = "enumValues",
+            deserialize_with = "option_pipe_separated_to_vec",
+            default
+        )]
+        enum_values: Option<Vec<SettingEnumValueInt>>,
     },
     Text {
         default: String,
         value: String,
-        #[serde(rename = "enumValues")]
-        enum_values: Option<String>, // TODO: decode values to hash/vec
+        #[serde(
+            rename = "enumValues",
+            deserialize_with = "option_pipe_separated_to_vec",
+            default
+        )]
+        enum_values: Option<Vec<SettingEnumValueString>>,
     },
     Double {
         // TODO: use f64, current problem that `TranscoderH264MinimumCRF` stored as string in JSON
--- plex-api/src/serde_helpers.rs
+++ plex-api/src/serde_helpers.rs
@@ -4,6 +4,7 @@ use std::str::FromStr;
 
 use chrono::{DateTime, Utc};
 use serde::de::{self, Deserializer};
+use std::iter::FromIterator;
 
 pub(crate) fn option_bool_from_int<'de, D>(deserializer: D) -> Result<Option<bool>, D::Error>
 where
@@ -25,16 +26,37 @@ pub(crate) fn option_seconds_to_datetime<'de, D>(d: D) -> Result<Option<DateTime
 where
     D: de::Deserializer<'de>,
 {
-    Ok(Option::from(chrono::serde::ts_seconds::deserialize(d)?))
+    chrono::serde::ts_seconds::deserialize(d).map(Option::from)
 }
 
-pub(crate) fn option_comma_separated_to_vec<'de, D, T>(d: D) -> Result<Option<Vec<T>>, D::Error>
+pub(crate) fn option_comma_separated_to_vec<'de, D, T, V>(d: D) -> Result<Option<T>, D::Error>
 where
     D: Deserializer<'de>,
-    T: FromStr,
-    T::Err: Display,
+    T: FromIterator<V>,
+    V: FromStr,
+    V::Err: Display,
 {
-    let v: Vec<T> =
-        serde_with::rust::StringWithSeparator::<serde_with::CommaSeparator>::deserialize(d)?;
-    Ok(Option::from(v))
+    serde_with::rust::StringWithSeparator::<serde_with::CommaSeparator>::deserialize::<D, T, V>(d)
+        .map(Option::from)
+}
+
+pub(crate) fn option_pipe_separated_to_vec<'de, D, T, V>(d: D) -> Result<Option<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: FromIterator<V>,
+    V: FromStr,
+    V::Err: Display,
+{
+    serde_with::rust::StringWithSeparator::<PipeSeparator>::deserialize::<D, T, V>(d)
+        .map(Option::from)
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct PipeSeparator;
+
+impl serde_with::Separator for PipeSeparator {
+    #[inline]
+    fn separator() -> &'static str {
+        "|"
+    }
 }
