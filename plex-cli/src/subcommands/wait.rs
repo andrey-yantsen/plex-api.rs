2988c14f86200fc4f87d1b1a39f928604312db09
Andrey Yantsen <andrey@janzen.su>
2020-01-18 17:41:55 +0000

    better output from plex-cli wait; fix an issue with missed sleep() call

--- plex-cli/src/subcommands/wait.rs
+++ plex-cli/src/subcommands/wait.rs
@@ -1,18 +1,18 @@
 use async_std::future;
 use clap::ArgMatches;
 use plex_api::Server;
+use std::thread;
 use std::time::Duration;
-use std::{thread, time};
 
 pub(crate) async fn subcommand_wait(
     token: &str,
     matches: Option<&ArgMatches<'_>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let matches = matches.unwrap();
-    let timeout: Duration =
-        Duration::from_secs(matches.value_of("timeout").unwrap_or("30").parse().unwrap());
+    let timeout = Duration::from_secs(matches.value_of("timeout").unwrap_or("30").parse().unwrap());
     let server_url = matches.value_of("server-url").unwrap();
     let wait_for_settings = matches.is_present("wait-for-settings");
+    let delay = Duration::from_secs(matches.value_of("delay").unwrap_or("30").parse().unwrap());
 
     let result = future::timeout(timeout, async {
         loop {
@@ -23,19 +23,31 @@ pub(crate) async fn subcommand_wait(
                     Server::connect_auth(server_url, token).await
                 }
             };
-            if let Ok(srv) = srv {
-                if wait_for_settings {
-                    if let Ok(settings) = srv.get_settings().await {
-                        if settings.get("AcceptedEULA").is_ok() {
-                            break;
+            match srv {
+                Ok(srv) => {
+                    if wait_for_settings {
+                        match srv.get_settings().await {
+                            Ok(settings) => match settings.get("AcceptedEULA") {
+                                Ok(..) => {
+                                    break;
+                                }
+                                Err(e) => {
+                                    eprintln!("AcceptedEULA setting not found: {}", e);
+                                }
+                            },
+                            Err(e) => {
+                                eprintln!("Unable to get settings: {}", e);
+                            }
                         }
+                    } else {
+                        break;
                     }
-                } else {
-                    break;
                 }
-            } else {
-                thread::sleep(time::Duration::from_secs(1));
+                Err(e) => {
+                    eprintln!("Failed to connect to the server: {}", e);
+                }
             }
+            thread::sleep(delay);
         }
     })
     .await;
