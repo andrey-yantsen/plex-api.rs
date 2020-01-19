bea8edeadb95a6219d86e73d4944ac898989e41b
Andrey Yantsen <andrey@janzen.su>
2020-01-19 18:52:54 +0000

    remove async-std dependency, thanks to reqwest we tied to tokio anyway

--- plex-cli/src/subcommands/wait.rs
+++ plex-cli/src/subcommands/wait.rs
@@ -1,8 +1,51 @@
-use async_std::future;
 use clap::ArgMatches;
 use plex_api::prelude::*;
 use std::thread;
 use std::time::Duration;
+use tokio::time;
+
+async fn wait(
+    token: &str,
+    server_url: &str,
+    wait_for_settings: bool,
+    delay: &Duration,
+) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        let srv = {
+            if token.is_empty() {
+                Server::connect(server_url).await
+            } else {
+                Server::connect_auth(server_url, token).await
+            }
+        };
+        match srv {
+            Ok(srv) => {
+                if wait_for_settings {
+                    match srv.get_settings().await {
+                        Ok(settings) => match settings.get("AcceptedEULA") {
+                            Ok(..) => {
+                                break;
+                            }
+                            Err(e) => {
+                                eprintln!("AcceptedEULA setting not found: {}", e);
+                            }
+                        },
+                        Err(e) => {
+                            eprintln!("Unable to get settings: {}", e);
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to connect to the server: {}", e);
+            }
+        }
+        thread::sleep(*delay);
+    }
+    Ok(())
+}
 
 pub(crate) async fn subcommand_wait(
     token: &str,
@@ -14,43 +57,7 @@ pub(crate) async fn subcommand_wait(
     let wait_for_settings = matches.is_present("wait-for-settings");
     let delay = Duration::from_secs(matches.value_of("delay").unwrap_or("3").parse().unwrap());
 
-    let result = future::timeout(timeout, async {
-        loop {
-            let srv = {
-                if token.is_empty() {
-                    Server::connect(server_url).await
-                } else {
-                    Server::connect_auth(server_url, token).await
-                }
-            };
-            match srv {
-                Ok(srv) => {
-                    if wait_for_settings {
-                        match srv.get_settings().await {
-                            Ok(settings) => match settings.get("AcceptedEULA") {
-                                Ok(..) => {
-                                    break;
-                                }
-                                Err(e) => {
-                                    eprintln!("AcceptedEULA setting not found: {}", e);
-                                }
-                            },
-                            Err(e) => {
-                                eprintln!("Unable to get settings: {}", e);
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Failed to connect to the server: {}", e);
-                }
-            }
-            thread::sleep(delay);
-        }
-    })
-    .await;
+    let result = time::timeout(timeout, wait(token, server_url, wait_for_settings, &delay)).await;
 
     if result.is_err() {
         Err(Box::new(result.err().unwrap()))
