7a2af3c6f22a16f1f835c856e4553f4649155d7a
Andrey Yantsen <andrey@janzen.su>
2020-06-06 22:33:17 +0100

    (library) add a ton of missed arguments to DirectoryMediaContainer

--- plex-api/src/media_container/library.rs
+++ plex-api/src/media_container/library.rs
@@ -1,4 +1,6 @@
 use crate::MediaContainer;
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize, Clone)]
 #[cfg_attr(all(test, feature = "test_new_attributes"), serde(deny_unknown_fields))]
@@ -33,8 +35,55 @@ impl From<LibraryMediaContainerOuter> for LibraryMediaContainer {
 
 #[derive(Debug, Deserialize, Clone)]
 #[cfg_attr(all(test, feature = "test_new_attributes"), serde(deny_unknown_fields))]
+#[serde(rename_all = "lowercase")]
+enum DirectoryType {
+    Movie,
+    Show,
+    #[serde(rename = "artist")]
+    Music,
+    Photo,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(all(test, feature = "test_new_attributes"), serde(deny_unknown_fields))]
+#[serde(rename_all = "camelCase")]
+struct DirectoryLocation {
+    id: u32,
+    path: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(all(test, feature = "test_new_attributes"), serde(deny_unknown_fields))]
 #[serde(rename_all = "camelCase")]
 pub struct DirectoryMediaContainer {
     pub(crate) key: String,
     pub(crate) title: String,
+    art: String,
+    allow_sync: bool,
+    composite: String,
+    filters: bool,
+    refreshing: bool,
+    thumb: String,
+    #[serde(rename = "type")]
+    directory_type: String,
+    agent: String,
+    scanner: String,
+    language: String,
+    uuid: Uuid,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    updated_at: DateTime<Utc>,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    created_at: DateTime<Utc>,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    scanned_at: DateTime<Utc>,
+    content: bool,
+    directory: bool,
+    content_changed_at: u64,
+    #[serde(deserialize_with = "serde_aux::prelude::deserialize_bool_from_anything")]
+    hidden: bool,
+    #[serde(default)]
+    #[serde(rename = "Location")]
+    location: Vec<DirectoryLocation>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    enable_auto_photo_tags: Option<bool>,
 }
