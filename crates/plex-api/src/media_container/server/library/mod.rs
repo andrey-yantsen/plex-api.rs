cb3eb90eb1a46cac4d78fcea4a1746912004d918
Andrey Yantsen <andrey-yantsen@users.noreply.github.com>
2023-04-09 11:57:05 +0100

    feat: add missing `thumb` property in Chapter

--- crates/plex-api/src/media_container/server/library/mod.rs
+++ crates/plex-api/src/media_container/server/library/mod.rs
@@ -574,6 +574,7 @@ pub struct Chapter {
     pub index: u32,
     pub start_time_offset: u64,
     pub end_time_offset: u64,
+    pub thumb: Option<String>,
 }
 
 pub(crate) fn deserialize_marker_type<'de, D>(deserializer: D) -> Result<MarkerType, D::Error>
