a921408df0494af5a968094467fb42fd6ef3d8fd
bors <26634292+bors@users.noreply.github.com>
2025-09-25 00:13:39 +0000

    chore: update Feature enum

--- crates/plex-api/src/media_container/server/feature.rs
+++ crates/plex-api/src/media_container/server/feature.rs
@@ -65,6 +65,7 @@ pub enum Feature {
         rename = "avod-ad-loudness",
         alias = "05826f20-284b-4bcb-b45c-2367e5c0ea72"
     )]
+    #[deprecated]
     AvodAdLoudness,
     #[serde(
         rename = "avod-new-media",
@@ -913,7 +914,6 @@ pub enum Feature {
         rename = "tvod",
         alias = "9b5a4bea-3bbe-45d2-b226-00a6ef4d8e65"
     )]
-    #[deprecated]
     Tvod,
     #[serde(
         rename = "tvod_playback",
@@ -1005,7 +1005,6 @@ pub enum Feature {
         rename = "vod_channels",
         alias = "5050545e-95c6-4a25-b94e-1fe5a5a603d0"
     )]
-    #[deprecated]
     VodChannels,
     #[serde(
         rename = "vod_cloudflare",
